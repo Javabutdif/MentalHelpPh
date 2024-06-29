5795567edc18d3122eeee0255f68a41dc3457339
Anton James Genabio <jamesgenabio31@gmail.com>
2024-06-29 21:34:24 +0800

    feat [James] : added navigation bar

--- index.php
+++ index.php
@@ -4,11 +4,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TheraMatch | Home</title>
+    <title>MentalHelp PH | Home</title>
+    <?php
+    require './assets/css/bootstrap.php';
+    ?>
 </head>
 
 <body>
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <img src="./assets/images/mentalhelp.jpg" style="height: 4rem ; width: 4rem" class="mx-3" />
+        <a class="navbar-brand" href="#">MentalHelp PH</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link " href="#">Home </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </body>
 
+
+<?php
+require './assets/js/bootstrap.php';
+?>
+
+
 </html>
+<script src="https://meet.jit.si/external_api.js"></script>
+
+<script>
+    const domain = 'meet.jit.si';
+    const options = {
+        roomName: 'PickAnAppropriateMeetingNameHere',
+        width: 700,
+        height: 700,
+        parentNode: document.querySelector('#meet')
+    };
+    const api = new JitsiMeetExternalAPI(domain, options);
+</script>
