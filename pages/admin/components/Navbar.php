9aba9d7d5bc5c83ba71bbce990bfe72c6d68c79e
Anton James Genabio <jamesgenabio31@gmail.com>
2024-09-21 23:50:33 +0800

    feat [James]: added gitignore

--- pages/admin/components/Navbar.php
+++ pages/admin/components/Navbar.php
@@ -1,31 +1,19 @@
 <nav class="navbar navbar-expand-lg " style="background-color:#144c94">
     <div class="container-fluid">
-        <a class="navbar-brand main-color" href="AdminDashboard.php">MentalHelp PH Dashboard</a>
+        <a class="navbar-brand text-white" href="AdminDashboard.php">MentalHelp PH Dashboard</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="index.php">Home</a>
+                    <a class="nav-link text-white" href="index.php">Patients</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Community
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Faculty Members</a>
-                    </div>
-                </li>
+
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#">About</a>
+                    <a class="nav-link text-white" href="#">Professionals</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="Login.php">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="Register.php">Register</a>
-                </li>
+
             </ul>
         </div>
     </div>
