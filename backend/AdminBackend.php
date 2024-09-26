f45ad48470b280f2a7d7d53af509c29ba7c52b0d
Jims <jamesgenabio31@gmail.com>
2024-09-26 17:03:51 +0800

    feat [James]: added dynamic table and responsive table

--- backend/AdminBackend.php
+++ backend/AdminBackend.php
@@ -1,4 +1,31 @@
 <?php
 
 require_once(__DIR__ . '/../connection/DatabaseConnection.php');
-?>
+
+
+function getAllPatients(){
+    $db = Database::getInstance();
+    $conn = $db->getConnection();
+
+    $sql = "SELECT patient.patient_id, patient.firstname, patient.lastname, patient.email, patient.addresses, patient.gender, patient.age, patient.patient_status, patient.contact_number FROM patient";
+
+  
+    $result = mysqli_query($conn, $sql);
+
+  
+    if (!$result) {
+       
+        die("Query Failed: " . mysqli_error($conn));
+    }
+
+    $listPatients = [];
+    
+    
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $listPatients[] = $row;
+        }
+    }
+
+    return $listPatients;
+}
--- pages/admin/Patients.php
+++ pages/admin/Patients.php
@@ -1,3 +1,9 @@
+<?php 
+    require_once '../components/TableComponent.php';
+    require_once '../../controller/AdminController.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,17 +11,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MentalHelp PH | Admin</title>
     <link rel="stylesheet" href="../../assets//css//styles.css">
+   
     <?php
     require_once '../../assets/css/bootstrap.php';
-  
+    require_once '../datatable_components/datatable_css.php';
      include '../components/Navbar.php';
     ?>
 </head>
 <body>
     <?php
     echo navbar("Patients" , "AdminDashboard.php","Dashboard",  "Patients" , "Professionals");
+
+   $header = ['patient_id', 'firstname', 'lastname', 'email' , "addresses" , "gender" , "age" , "patient_status" , "contact_number"];
+    $data =getAllPatients();
+
+    
     ?>
     
-    <h1>This is patients</h1>
+  
+    <div class="px-1 pt-3 table-responsive-xl">
+    <?php echo TableComponent($header, $data, "Edit", "Delete"); ?>
+    </div>
+
 </body>
+<?php 
+    require_once '../datatable_components/datatable_js.php';
+?>
+
+<script>
+  function DeleteButton(patient_id){
+    console.log("Delete button press" + patient_id);
+  }
+    </script>
+
+<script>
+    new DataTable('#example');
+    </script>
 </html>
